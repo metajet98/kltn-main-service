43c16dadf9007d16f5512b7d9aa0aaa86d935fdd
metajet98 <bac.nguyenxuan98@gmail.com>
2021-01-23 18:14:14 +0700

    update staff

--- Controllers/Staffs/StaffController.cs
+++ Controllers/Staffs/StaffController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace main_service.Controllers.Staffs
-{ 
+{
     [ApiController]
     [Route("/api/staff")]
     public class StaffController : ControllerBase
@@ -21,27 +21,27 @@ namespace main_service.Controllers.Staffs
         private readonly IEncryptionHelper _encryptionHelper;
         private readonly BranchRepository _branchRepository;
 
-        public StaffController(UserRepository userRepository, UserAuthRepository userAuthRepository, IEncryptionHelper encryptionHelper, BranchRepository branchRepository)
+        public StaffController(UserRepository userRepository, UserAuthRepository userAuthRepository,
+            IEncryptionHelper encryptionHelper, BranchRepository branchRepository)
         {
             _userRepository = userRepository;
             _userAuthRepository = userAuthRepository;
             _encryptionHelper = encryptionHelper;
             _branchRepository = branchRepository;
         }
-        
+
         [HttpPost]
-        [Route("maintenance")]
-        [Authorize(Roles = Constants.Role.CenterManager)]
-        public JsonResult CreateMaintenanceStaff([FromBody] UserRequest userRequest)
+        [Authorize(Roles = Role.CenterManager)]
+        public JsonResult CreateStaff([FromBody] UserRequest userRequest)
         {
             var newUser = new User
             {
                 Address = userRequest.Address,
-                Birthday = userRequest.Birthday, 
+                Birthday = userRequest.Birthday,
                 Email = userRequest.Email,
                 FullName = userRequest.FullName,
                 PhoneNumber = userRequest.PhoneNumber,
-                Role = Role.StaffMaintenance,
+                Role = userRequest.Role,
                 CreatedDate = DateTime.Now,
                 BranchId = userRequest.BranchId
             };
@@ -51,52 +51,29 @@ namespace main_service.Controllers.Staffs
             _userAuthRepository.Insert(newUserAuth);
             _userAuthRepository.Save();
 
-            return ResponseHelper<string>.OkResponse(null, "Tạo tài khoản thành công!");
-        }
-        
-        [HttpPost]
-        [Route("desk")]
-        [Authorize(Roles = Role.CenterManager)]
-        public JsonResult CreateDeskStaff([FromBody] UserRequest userRequest)
-        {
-            var newUser = new User
-            {
-                Address = userRequest.Address,
-                Birthday = userRequest.Birthday, 
-                Email = userRequest.Email,
-                FullName = userRequest.FullName,
-                PhoneNumber = userRequest.PhoneNumber,
-                Role = Constants.Role.StaffDesk,
-                CreatedDate = DateTime.Now,
-                BranchId = userRequest.BranchId
-            };
-            _userRepository.Insert(newUser);
-            _userRepository.Save();
-            var newUserAuth = _encryptionHelper.HashPassword(userRequest.Password, newUser.Id);
-            _userAuthRepository.Insert(newUserAuth);
-            _userAuthRepository.Save();
-            
             return ResponseHelper<string>.OkResponse(null, "Tạo tài khoản thành công!");
         }
 
         [HttpGet]
         [Authorize(Roles = Role.CenterManager)]
-        public JsonResult GetAll()
+        public JsonResult GetAll([FromQuery] int branchId)
         {
-            var staff = _userRepository.Get(x => x.Role.Equals(Role.StaffMaintenance) || x.Role.Equals(Role.StaffDesk), includeProperties: "Branch,Staff");
+            var staff = _userRepository.Get(
+                x => (x.Role.Equals(Role.StaffMaintenance) || x.Role.Equals(Role.StaffDesk)) &&
+                     x.BranchId.Equals(branchId), includeProperties: "Branch");
             return ResponseHelper<IEnumerable<User>>.OkResponse(staff);
         }
-        
+
         [HttpPost]
         [Route("{staffId}")]
         [Authorize(Roles = Role.CenterManager)]
         public JsonResult ModifyStaff(int staffId, [FromBody] BranchStaffRequest request)
         {
             var branch = _branchRepository.GetById(request.BranchId);
-            if(branch == null) return ResponseHelper<string>.ErrorResponse(null, "Chi nhánh không tồn tại!");
+            if (branch == null) return ResponseHelper<string>.ErrorResponse(null, "Chi nhánh không tồn tại!");
             var staff = _userRepository.Get(x => x.Id.Equals(staffId)).FirstOrDefault();
             if (staff == null) return ResponseHelper<string>.ErrorResponse(null, "Nhân viên!");
-            
+
             staff.BranchId = request.BranchId;
             _userRepository.Update(staff);
             _userRepository.Save();
--- RestApi/Requests/UserRequest.cs
+++ RestApi/Requests/UserRequest.cs
@@ -13,5 +13,6 @@ namespace main_service.RestApi.Requests
         public DateTime? Birthday { get; set; }
         public string Password { get; set; }
         public int? BranchId { get; set; }
+        public string Role { get; set; }
     }
 }
